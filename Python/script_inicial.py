b36c619cff052221a7c91a2a23099872a40ffbf6
Weelttt <ji.jeria.farias@gmail.com>
2026-07-18 11:18:29 -0400

    V2

--- Python/script_inicial.py
+++ Python/script_inicial.py
@@ -1,5 +1,4 @@
 
-Eleccion_Usuario = int(input('Hola, Que tipo de moneda desea convertir? \n1.Pesos Chilenos \n2.Soles Peruanos \n3.Reales Brasileños\n Seleccione una opción:'))
 
 Valor_dolar = 1
 
@@ -7,21 +6,36 @@ Pesos_valor =  924.5
 Soles_valor =  3.39 
 Reales_valor =  5.11 
 
+condicion = 1, 2, 3
 
-if Eleccion_Usuario == 1:
-    PesosUsuario = int(input('Cuanto tienes en pesos?'))
-    Conversion_pesos = PesosUsuario / Pesos_valor * Valor_dolar
-    print("Tus dolares actuales son:", f"{Conversion_pesos:.2f}", "USD")
+while condicion :
+
+    Eleccion_Usuario = int(input('Hola, Que tipo de moneda desea convertir? \n1.Pesos Chilenos \n2.Soles Peruanos \n3.Reales Brasileños\n4.Cerrar Programa \nSeleccione una opción:'))
+
+    if Eleccion_Usuario == 1:
+        PesosUsuario = int(input('Cuanto tienes en pesos? \n'))
+        Conversion_pesos = PesosUsuario / Pesos_valor * Valor_dolar
+        print("Tus dolares actuales son:", f"{Conversion_pesos:.2f}", "USD \n")
     
-elif Eleccion_Usuario == 2:
-    SolesUsuario = int(input('Cuanto tienes en soles?'))
-    Conversion_soles = SolesUsuario / Soles_valor * Valor_dolar
-    print("Tus dolares actuales son:", f"{Conversion_soles:.2f}","USD")
+    elif Eleccion_Usuario == 2:
+        SolesUsuario = int(input('Cuanto tienes en soles? \n'))
+        Conversion_soles = SolesUsuario / Soles_valor * Valor_dolar
+        print("Tus dolares actuales son:", f"{Conversion_soles:.2f}","USD \n")
 
-elif Eleccion_Usuario ==3:
-    RealesUsuario = int(input('cuanto tienes en reales?'))
-    Conversion_reales = RealesUsuario / Reales_valor * Valor_dolar
-    print("Tus dolares actuales son:", f"{Conversion_reales:.2f}","USD")
+    elif Eleccion_Usuario ==3:
+        RealesUsuario = int(input('cuanto tienes en reales? \n'))
+        Conversion_reales = RealesUsuario / Reales_valor * Valor_dolar
+        print("Tus dolares actuales son:", f"{Conversion_reales:.2f}","USD \n")
+    
+    elif Eleccion_Usuario == 4:
+        print("Gracias Por usar.")
+        break
+
+
+    else:
+        print("Por favor elija una opcion valida")
+
+         
 
 
 
