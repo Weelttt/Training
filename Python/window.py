407b9ca3c8e3d1b64395d4119e34b37c90ad4f2e
Weelttt <ji.jeria.farias@gmail.com>
2026-07-18 23:46:47 -0400

    window actualizado

--- Python/window.py
+++ Python/window.py
@@ -7,4 +7,14 @@ ventana.geometry("600x400+150+100")
 ventana.resizable(False,False)
 
 
+text = tk.Label(ventana, text="Hola")
+text.pack()
+
+def saludar():
+    text.config(text="Bienvenido👋")
+    
+button = tk.Button(ventana, text="Saludar", command=saludar)
+button.pack()
+    
+
 ventana.mainloop()
